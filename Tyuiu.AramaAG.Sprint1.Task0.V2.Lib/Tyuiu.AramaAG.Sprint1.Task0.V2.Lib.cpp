7f764ef8102416d575c8532ec9d5fa33798cac77
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-09-18 18:24:02 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.AramaAG.Sprint1.Task0.V2.Lib/Tyuiu.AramaAG.Sprint1.Task0.V2.Lib.cpp
@@ -0,0 +1,14 @@
+﻿// Tyuiu.AramaAG.Sprint1.Task0.V2.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "..//Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+// TODO: Это пример библиотечной функции.
+class Service : public ISprint1Task0V01
+{
+	virtual int Rezalt(int l, int w, int h) override
+	{
+		return 2 * h * (l + w);
+	}
+};
---
+++ Tyuiu.AramaAG.Sprint1.Task0.V2.Test/Tyuiu.AramaAG.Sprint1.Task0.V2.Test.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..//Tyuiu.AramaAG.Sprint1.Task0.V2.Lib/Tyuiu.AramaAG.Sprint1.Task0.V2.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Sprint0Task7V2Test
+{
+	TEST_CLASS(Sprint0Task1V2Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task0V01* service = new Service();
+			int l = 20;
+			int w = 10;
+			int h = 2;
+			int c = service->Rezalt(l, w, h);
+			Assert::AreEqual(120, c);
+		}
+	};
+}
